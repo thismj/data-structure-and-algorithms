2d602446cb9c524f74e2b822f75253df3c531271
ThisMJ <thiscrazymj@gmail.com>
2019-05-29 15:23:56 +0800

    - 分类:[数据结构与算法] - 改动:[增加单链表逆转]

--- data-structure/linked_list.h
+++ data-structure/linked_list.h
@@ -18,71 +18,61 @@ struct Node {
     Node<T> *next;
 };
 
+/**
+ * 单链表类定义
+ */
 template<class T>
 class LinkedList {
 public:
     LinkedList();
 
-    /**
-     * 插入数据到指定的位置
-     * @param index 指定位置
-     * @param data 数据
-     */
+    //插入数据到指定的位置
     void Insert(int index, T data);
 
-    /**
-     * 删除指定位置的数据
-     * @param index 指定位置
-     */
+    //删除指定位置的数据
     void Delete(int index);
 
-    /**
-     * 更新指定位置的数据
-     * @param index 指定位置
-     * @param data 新数据
-     */
-    void Edit(int index, T data);
+    //更新指定位置的数据
+    void Update(int index, T data);
 
-    /**
-     * @param index 指定位置
-     * @return 指定位置的数据
-     */
+    //获取指定位置的数据
     T Get(int index);
 
-    /**
-     * @return 元素总个数
-     */
+    //返回链表数据个数
     int Size();
 
-    /**
-     * 依次打印所有的数据
-     */
+    //反转单链表
+    void Reverse();
+
+    //依次打印所有的数据
     void Print();
 
 private:
-    /**
-     * @param index 指定位置
-     * @return 指定位置的结点
-     */
+    //返回指定位置的结点指针
     Node<T> *findNode(int index);
 
+    //头结点
     Node<T> *head;
+
+    //记录数据个数
     int size;
 };
 
 template<class T>
 LinkedList<T>::LinkedList(): size(0) {
-    head = new Node<T>;
-    head->next = NULL;
+    head = NULL;
 }
 
+/**
+ * 此方法用来返回单链表指定位置的结点指针，方便进行插入、删除、更新等操作
+ */
 template<class T>
 Node<T> *LinkedList<T>::findNode(int index) {
     if (index < 0 || index >= size) {
         throw std::invalid_argument("Find Error Index:" + to_string(index));
     }
 
-    Node<T> *current = head->next;
+    Node<T> *current = head;
     int position = 0;
     while (position < index) {
         current = current->next;
@@ -99,22 +89,18 @@ T LinkedList<T>::Get(int index) {
 
 template<class T>
 void LinkedList<T>::Insert(int index, T data) {
-    Node<T> *pre = NULL;
-    if (index == 0) {
-        pre = head;
-    } else {
-        pre = findNode(index - 1);
-    }
+    auto *node = new Node<T>();
+    node->data = data;
 
-    if (pre != NULL) {
-        auto *node = new Node<T>();
-        node->data = data;
+    if (index == 0) {
+        node->next = head;
+        head = node;
+    } else {
+        Node<T> *pre = findNode(index - 1);
         node->next = pre->next;
         pre->next = node;
-        size++;
-    } else {
-        throw std::invalid_argument("Insert Error Index: " + to_string(index));
     }
+    size++;
 }
 
 template<class T>
@@ -134,18 +120,19 @@ void LinkedList<T>::Delete(int index) {
 }
 
 template<class T>
-void LinkedList<T>::Edit(int index, T data) {
+void LinkedList<T>::Update(int index, T data) {
     Node<T> *current = findNode(index);
     current->data = data;
 }
 
 template<class T>
 void LinkedList<T>::Print() {
-    Node<T> *temp = head->next;
-    while (temp != NULL) {
-        cout << temp->data << endl;
+    Node<T> *temp = head;
+    while (temp) {
+        cout << temp->data << " ";
         temp = temp->next;
     }
+    cout << endl;
 }
 
 template<class T>
@@ -153,5 +140,26 @@ int LinkedList<T>::Size() {
     return size;
 }
 
+/**
+ * 通过遍历所有结点，反转单链表
+ */
+template<class T>
+void LinkedList<T>::Reverse() {
+    if (head == NULL || head->next == NULL) {
+        return;
+    }
+    Node<T> *pre = head;
+    Node<T> *current = head->next;
+    Node<T> *temp;
+    while (current) {
+        temp = current->next;
+        current->next = pre;
+        pre = current;
+        current = temp;
+    }
+
+    head->next = NULL;
+    head = pre;
+}
 
 #endif //DATA_STRUCTURE_AND_ALGORITHMS_LINKED_LIST_H
--- main.cpp
+++ main.cpp
@@ -5,19 +5,13 @@ using namespace std;
 
 int main() {
     LinkedList<int> list;
-    list.Insert(0,1);
-    list.Insert(1,2);
-    list.Insert(2,3);
-    list.Insert(3,4);
-    list.Insert(4,5);
-    list.Edit(0,22);
-    list.Edit(3,244);
-    list.Delete(3);
-    cout << "total size: " << list.Size() << endl;
+    list.Insert(0, 1);
+    list.Insert(1, 2);
+    list.Insert(2, 3);
+    list.Insert(3, 4);
+    list.Insert(4, 5);
     list.Print();
-    cout << "0:" << list.Get(0) << endl;
-    cout << "1:" << list.Get(1) << endl;
-    cout << "2:" << list.Get(2) << endl;
-    cout << "2:" << list.Get(3) << endl;
+    list.Reverse();
+    list.Print();
     return 0;
 }
